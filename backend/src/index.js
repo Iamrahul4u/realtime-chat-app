2a65b55517de661c53eb3e025c092aa9ad9b1230
Rahul Gupta <iamrahulgupta4u@gmail.com>
2025-01-11 15:12:48 +0530

    config

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,9 @@ import messageRouter from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { io, app, server } from "./socket.js";
+import path from "path";
 dotenv.config();
-
+const __dirname = path.resolve();
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -27,6 +28,12 @@ app.use(express.json({ limit: "10mb" }));
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/messages", messageRouter);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 server.listen(process.env.PORT, () => {
   console.log("server is running on port 5001");
   connectDB();
